578b7f27e7c27badfcde6d59f0e80b41d02997ac
Shane Martin <shane.martin@rewardops.com>
2019-11-20 09:03:43 -0500

    chore: add jest eslint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     node: true,
     jest: true,
   },
-  extends: ['airbnb-base', 'plugin:prettier/recommended'],
+  extends: ['airbnb-base', 'plugin:prettier/recommended', 'plugin:jest/recommended'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
