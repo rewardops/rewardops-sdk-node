88e7ee24f52df8298e138cd602f3287b2e9d3aad
Alex Cheng <achen080@uottawa.ca>
2020-09-25 09:35:36 -0400

    test: add missing tests for auth.get token [MX-1108] (#41)
    * refactor: rename options in getToken to config
    
    * refactor: rename setV5Token func to setPiiToken
    
    * docs: add JSDocs to base auth methods that take arguments
    
    * refactor: move `getTestAccessToken` to separate file
    
    * test: add `mockConfig` factory test helper
    
    * test: adding missing test for `auth.getToken`
    
    * test: remove unnecessary mock call
    
    * docs: add docs for `mockConfig`
    
    * docs: correct docs for `mockConfig`
    
    * refactor: change filename to kebab-case
    
    Co-authored-by: Alex Cheng <alex.cheng@rewardops.com>

---
+++ test/test-helpers/auth-helpers.js
@@ -0,0 +1,16 @@
+const faker = require('faker');
+
+/**
+ * Since OAuth Bearer tokens don't have a defined structure, we will
+ * generate alphanumeric strings of length 24.
+ *
+ * @see {@link https://www.oauth.com/oauth2-servers/access-tokens/access-token-response/ OAuth access tokens}
+ *
+ * @returns {string} testAccessToken
+ *
+ * @example
+ * getTestAccessToken() // => mjpyxya9signd3g9zu1s1zkb
+ */
+const getTestAccessToken = () => faker.random.alphaNumeric(24);
+
+module.exports = { getTestAccessToken };
--- test/utils/axios-helper.test.js
+++ test/utils/axios-helper.test.js
@@ -5,6 +5,7 @@ const faker = require('faker');
 const config = require('../../lib/config');
 const auth = require('../../lib/auth');
 const { setPiiToken } = require('../../lib/utils/axios-helpers');
+const { getTestAccessToken } = require('../test-helpers/auth-helpers');
 
 const RoUrl = faker.internet.url();
 const piiUrl = faker.internet.url();
@@ -15,21 +16,6 @@ config.set('apiServerUrl', RoUrl);
 config.set('piiServerUrl', piiUrl);
 config.set('clientId', clientId);
 config.set('clientSecret', clientSecret);
-
-// test helpers
-
-/**
- * Since OAuth Bearer tokens don't have a defined structure, we will
- * generate alphanumeric strings of length 24.
- *
- * @see {@link https://www.oauth.com/oauth2-servers/access-tokens/access-token-response/ OAuth access tokens}
- *
- * @returns {string} testAccessToken
- *
- * @example
- * getTestAccessToken() // => mjpyxya9signd3g9zu1s1zkb
- */
-const getTestAccessToken = () => faker.random.alphaNumeric(24);
 
 const mockV5AuthAPI = () =>
   nock(piiUrl)
