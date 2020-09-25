6a94b7aab00b0b59fb618f913f09abc99cce6327
Alex Cheng <alex.cheng@rewardops.com>
2020-09-25 08:18:27 -0400

    docs: correct docs for `mockConfig`

--- test/test-helpers/mockConfig.js
+++ test/test-helpers/mockConfig.js
@@ -6,8 +6,8 @@ const { getTestAccessToken } = require('../test-helpers/auth-helpers');
  * Test helper that returns a mock config object.
  *
  *
- * @param {{apiServerUrl: string, apiVersion: string, piiServerUrl: string, clientId: string, clientSecret: string, logToFile: boolean, timeout: number, verbose: boolean, supportedLocales: Array|undefined}} configOverrides Request retry strategy options.
- * @returns {module:api~requestCallback} callback Callback that handles the response.
+ * @param {{apiServerUrl: string, apiVersion: string, piiServerUrl: string, clientId: string, clientSecret: string, logToFile: boolean, timeout: number, verbose: boolean, supportedLocales: Array|undefined}} configOverrides Returns a complete config object with overrides and defaults.
+ * @returns {object} Mock config object for tests.
  *
  * @protected
  */
