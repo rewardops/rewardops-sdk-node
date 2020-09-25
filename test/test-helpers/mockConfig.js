5cd4b914d69cda90e15e33fe4c4f1a07e57dccde
Alex Cheng <alex.cheng@rewardops.com>
2020-09-25 08:16:06 -0400

    docs: add docs for `mockConfig`

--- test/test-helpers/mockConfig.js
+++ test/test-helpers/mockConfig.js
@@ -2,6 +2,15 @@ const faker = require('faker');
 
 const { getTestAccessToken } = require('../test-helpers/auth-helpers');
 
+/**
+ * Test helper that returns a mock config object.
+ *
+ *
+ * @param {{apiServerUrl: string, apiVersion: string, piiServerUrl: string, clientId: string, clientSecret: string, logToFile: boolean, timeout: number, verbose: boolean, supportedLocales: Array|undefined}} configOverrides Request retry strategy options.
+ * @returns {module:api~requestCallback} callback Callback that handles the response.
+ *
+ * @protected
+ */
 const mockConfig = ({
   apiServerUrl = faker.internet.url(),
   apiVersion = 'v4',
